0eb838c9537853bbb3d63b2b7ac53ff8eeaeb9d6
Nijisha <Nijisha@users.noreply.github.com>
2022-01-17 00:49:49 +0530

    Update publish.groovy

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -1,7 +1,10 @@
 def call(opts) {
+   pipeline {
+       agent any
+     stage{
   stage('Build') {
     steps{
   echo "env is ${env.MAIN_SEDM}"
             sh  echo ${env.MAIN_SEDM}
-    }}
-}
+    }}}
+   }}
