9bf576116a56c60e7e603c5953c559ebf7feeb16
Nijisha <Nijisha@users.noreply.github.com>
2022-01-17 00:37:05 +0530

    Update publish.groovy

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -1,8 +1,7 @@
-import jenkins.model.*
-jenkins = Jenkins.instance
 def call(opts) {
   stage('Build') {
-  sh echo "env is ${env.MAIN_SEDM}"
+    steps{
+  echo "env is ${env.MAIN_SEDM}"
             sh  echo ${env.MAIN_SEDM}
-        }
+    }}
 }
