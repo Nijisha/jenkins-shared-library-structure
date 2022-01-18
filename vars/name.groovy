59cd0dd9f09b5be3705be4dc1a050a2482d56dbd
Nijisha <Nijisha@users.noreply.github.com>
2022-01-18 21:05:47 +0530

    Update name.groovy

--- vars/name.groovy
+++ vars/name.groovy
@@ -1,13 +1,11 @@
 def call(Map message) {
     echo "before changing"
-    echo message
     echo (currentBuild.projectName)
 
     jobName([        
         (currentBuild.projectName): [[(currentBuild.projectName):  message.JOB_BASE_NAME ]]
     ])
     echo "after changing"
-    echo message
     echo (currentBuild.projectName)
     
   }
