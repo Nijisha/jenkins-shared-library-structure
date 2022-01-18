88120b01c9cbe2d671d472e965eb9949f5677114
Nijisha <Nijisha@users.noreply.github.com>
2022-01-18 21:09:39 +0530

    Update name.groovy

--- vars/name.groovy
+++ vars/name.groovy
@@ -1,6 +1,11 @@
 def call(Map message) {
     echo "before changing"
     echo (currentBuild.projectName)
+    item = Jenkins.instance.getItemByFullName((currentBuild.projectName))
+    item.save()
+    item.renameTo(message.JOB_BASE_NAME)
+    
+
 
     jobName([        
         (currentBuild.projectName): [[(currentBuild.projectName):  message.JOB_BASE_NAME ]]
