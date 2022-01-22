352b77777ded02d5581bdfeb1c9a34346dcf1e06
Nijisha <Nijisha@users.noreply.github.com>
2022-01-23 00:14:04 +0530

    Create request.groovy

---
+++ vars/request.groovy
@@ -0,0 +1,17 @@
+def call(Map params) {
+	def requestContent = new StringBuilder()
+    process([
+		requestContent.append('{')
+		requestContent.append("\"applicationCode\" : \"params.applicationCode\",")
+		requestContent.append("\"applBranchName\" : \"params.applBranchName\",")
+		requestContent.append("\"changemanPackageNumber\" : \"params.changemanPackageNumber\",") 
+		requestContent.append("\"gexxBranchName\" : \"params.gexxBranchName\",")    	            
+		
+		requestContent.append("\"userid\" : \"params.userid\",")
+		requestContent.append("\"callbackURL\" : \"params.callbackURL\"}") 
+						
+		echo "requestBody requestContent.toString()"
+        
+    ])
+    
+  }
