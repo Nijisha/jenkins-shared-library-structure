6ff92301c231438498f88d89fcc1df9e5589c6b9
Nijisha <Nijisha@users.noreply.github.com>
2022-01-23 00:41:41 +0530

    Update request.groovy

--- vars/request.groovy
+++ vars/request.groovy
@@ -1,16 +1,8 @@
 def call(Map params) {
 	def requestContent = new StringBuilder()
     process([
-		requestContent.append('{')
-		requestContent.append("\"applicationCode\" : \"params.applicationCode\",")
-		requestContent.append("\"applBranchName\" : \"params.applBranchName\",")
-		requestContent.append("\"changemanPackageNumber\" : \"params.changemanPackageNumber\",") 
-		requestContent.append("\"gexxBranchName\" : \"params.gexxBranchName\",")    	            
-		
-		requestContent.append("\"userid\" : \"params.userid\",")
-		requestContent.append("\"callbackURL\" : \"params.callbackURL\"}") 
-						
-		echo "requestBody requestContent.toString()"
+	    json.append("""{ "applicationCode": "params.applicationCode", "applBranchName": "params.applBranchName", "changemanPackageNumber": "params.changemanPackageNumber", "gexxBranchName": "params.gexxBranchName", "userid": "params.userid", "callbackURL": "params.callbackURL" },""");
+	    echo "requestBody requestContent.toString()"
         
     ])
     
